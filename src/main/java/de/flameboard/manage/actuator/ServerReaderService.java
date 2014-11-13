d3d2f3f241ffd6d35b71d8e582da0f8b11ba0e64
sven klemmer <svenklemmer@aol.com>
2014-11-13 19:07:29 +0100

    iniit

--- src/main/java/de/flameboard/manage/actuator/ServerReaderService.java
+++ src/main/java/de/flameboard/manage/actuator/ServerReaderService.java
@@ -3,6 +3,14 @@ package de.flameboard.manage.actuator;
 import org.apache.log4j.Logger;
 
 /**
+ * service that is capable of calling a execute method of each service
+ * which will get the information from routes push them to a equivalent
+ * holder object and passes them to the dashboard controller endpoint
+ *
+ * @see Service1
+ * @see Service1
+ * @see Service1
+ *
  * @author svenklemmer
  * @since 1.0.0
  */
